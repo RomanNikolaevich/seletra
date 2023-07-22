b5ec9ecb5a4a88a5afd15c0bcddc17dc78066f4c
Admin <roman.nikolaevich@gmail.com>
2023-07-22 14:25:04 +0300

    feat: fas icon components
    add StartEdit and EndEdit component with fas icons

---
+++ resources/js/Components/Fas/StartEditPen.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import {ref} from "vue";
+
+const isHovered = ref(false);
+</script>
+
+<template>
+    <font-awesome-icon
+        :icon="['fas', 'pen']"
+        :class="{'fa-bounce': isHovered}"
+        @mouseover="isHovered = true"
+        @mouseleave="isHovered = false"
+        size="lg" style="color: #0f0f10;"
+    />
+</template>
