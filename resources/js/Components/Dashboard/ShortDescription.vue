83aace6ce2a6a004bcb8c1e2197f25d72c80fbbc
Admin <roman.nikolaevich@gmail.com>
2023-07-24 23:59:04 +0300

    feat: add ShortDescription
    add ShortDescription component

---
+++ resources/js/Components/Dashboard/ShortDescription.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import {computed, ref} from "vue";
+
+const props = defineProps({
+    course: Object,
+    textLength: Number,
+    blackText: Boolean,
+});
+
+const showFullText = ref(false);
+
+const isShortText = computed(() => props.course.description.length <= props.textLength);
+
+const truncatedDescription = computed(() => {
+    if (showFullText.value) {
+        return props.course.description;
+    } else if (isShortText.value) {
+        return props.course.description;
+    } else {
+        return props.course.description.slice(0, props.textLength) + " ...";
+    }
+});
+</script>
+
+<template>
+    <span class="font-medium text-lg">{{ truncatedDescription }}</span>
+
+    <template v-if="!isShortText">
+        <template v-if="!showFullText">
+            <button
+                @click="showFullText = true"
+                :class="{ 'text-blue-700': blackText, 'text-white': !blackText }"
+                class="ml-3 font-extrabold text-2xl"
+            >
+                <span class="text-2xl">+</span>
+            </button>
+        </template>
+        <template v-else>
+            <button
+                @click="showFullText = false"
+                :class="{ 'text-blue-700': blackText, 'text-white': !blackText }"
+                class="ml-3 font-extrabold text-2xl"
+            >
+                <span class="text-2xl">&ndash;</span>
+            </button>
+        </template>
+    </template>
+</template>
--- resources/js/Pages/Courses/Partials/IndexCourseItem.vue
+++ resources/js/Pages/Courses/Partials/IndexCourseItem.vue
@@ -3,10 +3,13 @@ import {Link} from "@inertiajs/vue3";
 import IndexCourseItemSummary from "@/Pages/Courses/Partials/IndexCourseItemSummary.vue";
 import CourseType from "@/Components/Fas/CourseType.vue";
 import IndexCourseItemBody from "@/Pages/Courses/Partials/IndexCourseItemBody.vue";
+import ShortDescription from "@/Components/Dashboard/ShortDescription.vue";
 
 const props = defineProps({
     course: Object,
 });
+
+const blackText = (props.course.status_id === 1 || props.course.status_id === 2 || props.course.status_id === 3);
 
 const formatDateTime = (dateTime) => {
     const options = {
@@ -30,13 +33,17 @@ const formatDateTime = (dateTime) => {
         </IndexCourseItemSummary>
         <IndexCourseItemBody :course="course">
             <p>
-                <span class="font-extrabold">Description: </span> {{ props.course.description }}
+                <ShortDescription
+                    :course="course"
+                    :textLength="150"
+                    :blackText="blackText"
+                />
             </p>
             <p>
                 <span class="font-extrabold">Created: </span>{{ formatDateTime(props.course.created_at) }}
             </p>
             <p>
-                <Link v-if="course.status_id === 1 || course.status_id === 2 || course.status_id === 3"
+                <Link v-if="blackText"
                       :href="route('courses.show', { id: course.id })"
                       class="font-semibold text-indigo-700  hover:text-indigo-900"
                 >Read more ...
